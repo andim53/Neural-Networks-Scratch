dedf725d0167d0a552c1cff7039260c3ea47e53e
Andi <andimuhammad164@gmail.com>
2024-10-23 19:17:07 +0900

    commit

--- train_model.py
+++ train_model.py
@@ -5,6 +5,13 @@ data = pd.read_csv("./dataset/train.csv")
 data = np.array(data)
 m, n = data.shape # row and column
 
+data_train = data[1000:m].T
+Y_train = data_train[0]
+X_train = data_train[1:n] # Feature, number of data
+
+X_train = X_train / 255.
+_,m_train = X_train.shape
+
 # Def params
 def init_params(layers, activation_func):
     params = {}
@@ -35,7 +42,7 @@ def forward_prop(params, X):
     # Initialize input
     A = X
     
-    num_layers = len(params) // 2
+    num_layers = sum(1 for key in params.keys() if key.startswith('W'))  # Count weights to determine the number of layers
     
     # Initialize Z and A
     Z_vals = []
@@ -60,10 +67,52 @@ def forward_prop(params, X):
             A = Z
         
         A_vals.append(A)
+    return Z_vals, A_vals
+
+# Backward propagation
+def backward_prop(Z_vals, A_vals, params, X, Y):
+    
+    m = Y.size # Number of data
+    one_hot_Y = one_hot(Y)
+    
+    # Initialize dict to hold grad for weight and bias
+    dW = {}
+    db = {}
+    
+    # Compute grad for the last layer
+    A_last = A_vals[-1]
+    dZ_last = A_last - one_hot_Y # Derivative of the output layer
+    
+    num_layers = sum(1 for key in params.keys() if key.startswith('W'))
+    
+    # Backpropagation for the last layer
+    W_last = params[f'W{num_layers}']
+    dW[num_layers] = (1 / m) * dZ_last.dot(A_vals[-2].T)  # Gradient for weights of the last layer
+    db[num_layers] = (1 / m) * np.sum(dZ_last, axis=1, keepdims=True)  # Gradient for biases of the last layer
+
+    
+
+
+# One Hot encoding
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    
+    return one_hot_Y
+
+# Derivative
+def ReLU_deriv(Z):
+    return Z > 0
         
 
-layer_sizes = [784, 10, 10] # First layer has 784 neuron, second layer has 10 neuron, third layer has 10 neuron. First layer is the input layer, last layer is the output layer.
-activation_func = ['ReLu', 'softmax']
+layer_sizes = [n-1, 100, 100, 10] # First layer has 784 neuron, second layer has 10 neuron, third layer has 10 neuron. First layer is the input layer, last layer is the output layer.
+# gives n-1, so that it could match the size of the input feature
+
+activation_func = ['ReLu','ReLu','softmax']
 params = init_params(layer_sizes, activation_func)
+
+num_layers = sum(1 for key in params.keys() if key.startswith('W'))  # Count weights to determine the number of layers
+
 
 print(params['W1'])
